e391aa6c8be820208088087faa0ebe048536afa2
George Soulantikas <soulgeo01@gmail.com>
2026-05-30 09:48:13 +0300

    remove legacy links

--- frontend/src/components/layout/Navbar/Navigation.tsx
+++ frontend/src/components/layout/Navbar/Navigation.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router";
 import { useAuth } from "../../../context/AuthContext";
 
 const Navigation = () => {
@@ -8,12 +7,6 @@ const Navigation = () => {
     <>
       {userLoggedIn && (
         <div className="flex flex-row gap-2">
-          <Link to="/dashboard" className="btn btn-ghost">
-            Dashboard
-          </Link>
-          <Link to="/continue" className="btn btn-ghost">
-            Continue Learning
-          </Link>
         </div>
       )}
     </>
